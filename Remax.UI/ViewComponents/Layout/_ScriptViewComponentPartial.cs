1626602c0ba9443f070c5fd80ae1fcae24186993
altanemredemirci <altan_emre_1989@hotmail.com>
2024-05-04 17:07:35 +0300

    FreeTemplate_Makaan al Template i Layout'a uyarla Layout sayfasının ViewComponent'e uyarla _HeaderViewComponentPartial.cs _NavbarViewComponentPartial.cs _FooterViewComponentPartial.cs _ScriptViewComponentPartial.cs

---
+++ Remax.UI/ViewComponents/Layout/_ScriptViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Remax.UI.ViewComponents.Layout
+{
+    public class _ScriptViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
