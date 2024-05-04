1a358fc6b2ace899dc28ac1d4af551e2669716f7
101-Hoca-Pc <101-Hoca-Pc@DESKTOP-58CMK8T>
2024-05-04 16:45:41 +0300

    Add project files.

---
+++ Remax.UI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Remax.UI.Models;
+using System.Diagnostics;
+
+namespace Remax.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
